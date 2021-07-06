b62c1b81e20dd90c8f4af03145d35f1d70ddba3b
dishadutta <duttadisha12345@gmail.com>
2021-07-06 14:56:37 +0530

    Updates

---
+++ precache-manifest.c33a5047f4199185a22f18f9469b456c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f68eb0cbf68d1a89ab43355280611fa",
+    "url": "/Flexshop/index.html"
+  },
+  {
+    "revision": "f2c4fa9c93ae2ab295c5",
+    "url": "/Flexshop/static/css/main.ead9d38f.chunk.css"
+  },
+  {
+    "revision": "70cd0d3b77655a391135",
+    "url": "/Flexshop/static/js/2.60327f06.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Flexshop/static/js/2.60327f06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2c4fa9c93ae2ab295c5",
+    "url": "/Flexshop/static/js/main.4a0bc0ad.chunk.js"
+  },
+  {
+    "revision": "e662499642909ccf0f5e",
+    "url": "/Flexshop/static/js/runtime-main.a53eb434.js"
+  },
+  {
+    "revision": "5caa2bdc311214f2cca31bac8b688803",
+    "url": "/Flexshop/static/media/ad.5caa2bdc.PNG"
+  },
+  {
+    "revision": "e317b7c9f02d84bbfa2e97848c672372",
+    "url": "/Flexshop/static/media/brand1.e317b7c9.PNG"
+  },
+  {
+    "revision": "518ceb11e966ebafe390a402f12085d0",
+    "url": "/Flexshop/static/media/brand2.518ceb11.PNG"
+  },
+  {
+    "revision": "9a2d4ec4bb274984c20cd643f671541c",
+    "url": "/Flexshop/static/media/car.9a2d4ec4.PNG"
+  },
+  {
+    "revision": "5e0f70f81254cc2aa079a28614325a6c",
+    "url": "/Flexshop/static/media/card.5e0f70f8.PNG"
+  },
+  {
+    "revision": "94e128997aa4b53db3fb4622c9e99078",
+    "url": "/Flexshop/static/media/cardboard.94e12899.PNG"
+  },
+  {
+    "revision": "6b97bd0e784c11154c4a7da4f702fc21",
+    "url": "/Flexshop/static/media/cycle.6b97bd0e.PNG"
+  },
+  {
+    "revision": "2aea9ecd8622f7181b093cc50b4baf10",
+    "url": "/Flexshop/static/media/drum.2aea9ecd.PNG"
+  },
+  {
+    "revision": "eb5e8ea92a396a57f2c87239a5b962cc",
+    "url": "/Flexshop/static/media/headphone.eb5e8ea9.PNG"
+  },
+  {
+    "revision": "7f0821f71088c804f0ae1d87d2524e0e",
+    "url": "/Flexshop/static/media/helicopter.7f0821f7.PNG"
+  },
+  {
+    "revision": "c12c7c7d57f3082758bce5c85a8ce4d4",
+    "url": "/Flexshop/static/media/house.c12c7c7d.PNG"
+  },
+  {
+    "revision": "e6db82ab3fd5ffdf091f570832bbfc8f",
+    "url": "/Flexshop/static/media/laptop.e6db82ab.PNG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Flexshop/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "563cb37afd478f615b9944074bf56a3a",
+    "url": "/Flexshop/static/media/mf.563cb37a.PNG"
+  },
+  {
+    "revision": "66a270e402b53aff9982f27d53839241",
+    "url": "/Flexshop/static/media/mobile.66a270e4.PNG"
+  },
+  {
+    "revision": "cea53cd6d3ff9bbd96a90439b1c6bbb3",
+    "url": "/Flexshop/static/media/slide1.cea53cd6.PNG"
+  },
+  {
+    "revision": "8aff880596101c4dc17597ddb5a96a21",
+    "url": "/Flexshop/static/media/slide2.8aff8805.PNG"
+  },
+  {
+    "revision": "f885abd5f91faa1fbc5cc65522afe3f6",
+    "url": "/Flexshop/static/media/slide3.f885abd5.PNG"
+  }
+]);
